30946e8f05abfed2d2a4fac02d029fb1a093949a
ccosnett <conor142857@icloud.com>
2026-03-22 16:40:03 +0000

    change back ground to green

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d4f1c4',
     alignItems: 'center',
     justifyContent: 'center',
   },
